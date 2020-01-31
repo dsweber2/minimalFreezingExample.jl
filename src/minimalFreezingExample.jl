4ed03779b8dd635145bb815ffd7b58eb16bbbade
dsweber <david.weber2@gmail.com>
2020-01-30 17:31:00 -0800

    Files generated by PkgTemplates

---
+++ src/minimalFreezingExample.jl
@@ -0,0 +1,5 @@
+module minimalFreezingExample
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using minimalFreezingExample
+using Test
+
+@testset "minimalFreezingExample.jl" begin
+    # Write your own tests here.
+end
