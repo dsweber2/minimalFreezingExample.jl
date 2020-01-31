e1585f584563c9451f8313efb1602a1af6014ff5
dsweber <david.weber2@gmail.com>
2020-01-30 18:16:55 -0800

    (dis)functional example

--- src/minimalFreezingExample.jl
+++ src/minimalFreezingExample.jl
@@ -1,5 +1,51 @@
 module minimalFreezingExample
+  using Distributed, FFTW
+  export createSeveralRemotes, createFFTPlans
+  function createSeveralRemotes(k, sizeToMake)
+      FFTs = Array{Future,2}(undef, nworkers(), k)
+      for i=1:nworkers(), j=1:k
+          FFTs[i, j] = remotecall(makeRemoteArray, i, sizeToMake)
+      end
+      return FFTs
+  end
+  function makeRemoteArray(sizeToMake)
+      return randn(sizeToMake)
+  end
 
-greet() = print("Hello World!")
 
-end # module
+  function createFFTPlans(m,n, dataSizes;
+                        verbose=false, iscomplex::Bool = false) where {N}
+    nPlans = 1
+    FFTs = Array{Future,2}(undef, nworkers(), m+1)
+    for i=1:nworkers(), j=1:m+1
+        if verbose
+            println("i=$(i), j=$(j)")
+        end
+        if length(n) >= 2
+            FFTs[i, j] = remotecall(createRemoteFFTPlan, i,
+                                    dataSizes[j],
+                                    iscomplex, nPlans)
+        else
+            fftPlanSize = (dataSizes[j][1]*2, dataSizes[j][3:end]...)
+            FFTs[i, j] = remotecall(createRemoteFFTPlan, i, fftPlanSize,
+                                    iscomplex, nPlans)
+        end
+    end
+
+    return FFTs
+end
+
+
+function createRemoteFFTPlan(sizeOfArray, iscomplex, nPlans::Int)
+    if nPlans==1
+        return plan_rfft(zeros(sizeOfArray...), (1,), flags =
+                         FFTW.PATIENT)
+    elseif nPlans==2
+        return (plan_rfft(zeros(T, sizeOfArray...), (1,), flags =
+                          FFTW.PATIENT),
+                plan_fft!(zeros(sizeOfArray...), (1,), flags =
+                         FFTW.PATIENT))
+    end
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
-using minimalFreezingExample
+using Distributed
+addprocs((Sys.CPU_THREADS)-2-nprocs())
+@everywhere using minimalFreezingExample
 using Test
 
 @testset "minimalFreezingExample.jl" begin
-    # Write your own tests here.
+    @test size(createSeveralRemotes(5, (200, 30, 10))) == (5, 5)
+    println("first one fine")
+    @test size(createFFTPlans(2, [1,2,3], [(200, 30, 10), (200, 30, 10), (200, 30, 10), (200, 30, 10)])) == (5, 5)
+    println("second one made it")
 end
